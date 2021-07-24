16be1d8a8901187e69130caf4e299e1c34c8bdcc
LSOffice <lsoffice.noreply@gmail.com>
2021-07-24 18:35:50 +0800

    changed start.sh #1

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 export SPOTIPY_CLIENT_ID='ab487ce8b7394857b7131f62e83198be'
 export SPOTIPY_CLIENT_SECRET='0c7bd99559c24d0994271fd2efcfe01a'
 export SPOTIPY_REDIRECT_URI='http://127.0.0.1/callback'
+cd ./BetterSpotify/
 pythonw main.py
