24f0a8c0223edddcc71af425efe622904ff1636a
LSOffice <lsoffice.noreply@gmail.com>
2021-07-24 22:16:28 +0800

    Changes

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 export SPOTIPY_CLIENT_ID='ab487ce8b7394857b7131f62e83198be'
 export SPOTIPY_CLIENT_SECRET='0c7bd99559c24d0994271fd2efcfe01a'
-export SPOTIPY_REDIRECT_URI='http://127.0.0.1/callback'
+export SPOTIPY_REDIRECT_URI='http://127.0.0.1:4321/callback'
 
 python3 -m pip install spotipy --user --upgrade
 python3 -m pip install wxPython --user --upgrade
 
 cd ./BetterSpotify/
-pythonw main.pyw
+python3 main.py
