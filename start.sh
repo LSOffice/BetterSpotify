28cd3e49489366627f691a61a1a4d637cfed73f4
LSOffice <lsoffice.noreply@gmail.com>
2021-07-24 20:57:47 +0800

    Pyw and start.sh changes

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 export SPOTIPY_CLIENT_ID='ab487ce8b7394857b7131f62e83198be'
 export SPOTIPY_CLIENT_SECRET='0c7bd99559c24d0994271fd2efcfe01a'
 export SPOTIPY_REDIRECT_URI='http://127.0.0.1/callback'
+
+python3.8 -m pip install spotipy --user --upgrade
+python3.8 -m pip install wxPython --user --upgrade
+
 cd ./BetterSpotify/
-pythonw main.py
+python3.8 main.pyw
